7796557765792682386045bce1b958fbc502be3f
Matt Johanson <matt.johanson6@gmail.com>
2024-04-18 16:52:39 -0500

    Enhancement: Implement Pixel Art Effect Through Upscaling Rendered Sprite
    In main.py, the .Surface function is utilized to render our sprite on a half-resolution display. Subsequently, this rendered sprite is upscaled to fit the larger resolution screen. This deliberate approach results in the enlargement of the sprite, effectively creating a pixel art effect. This enhancement adds visual interest and character to the game's graphics, contributing to its aesthetic appeal.

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ class Game:
 
         pygame.display.set_caption('ninja game')
         self.screen = pygame.display.set_mode((640, 480))
+        self.display = pygame.Surface((320,240))
 
         self.clock = pygame.time.Clock()
 
@@ -22,10 +23,10 @@ class Game:
 
     def run(self):
         while True:
-            self.screen.fill((14, 219, 248))
+            self.display.fill((14, 219, 248))
 
             self.player.update((self.movement[1] - self.movement[0], 0))
-            self.player.render(self.screen)
+            self.player.render(self.display)
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -42,6 +43,7 @@ class Game:
                     if event.key == pygame.K_RIGHT:
                         self.movement[1] = False
 
+            self.screen.blit(pygame.transform.scale(self.display, self.screen.get_size()), (0,0))
             pygame.display.update()
             self.clock.tick(60)
 
