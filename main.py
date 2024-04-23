f3ca0d027b2d8467815b661880719b88f2ee4d56
Matt Johanson <matt.johanson6@gmail.com>
2024-04-23 13:37:26 -0500

    Feature Addition: Implement Tilemap Generation and Rendering
    This commit introduces a new script, tilemap.py, dedicated to generating both on-grid and off-grid tiles and rendering them within the game environment. Additionally, a load_images function is incorporated into the utils.py script to efficiently load multiple images and store them in an 'images' list.
    
    Several assets including decor, grass, large_decor, and stone are added to the main.py assets folder to enrich the game's visual elements.
    
    In main.py, a tilemap is created and subsequently rendered on the display. This comprehensive addition lays the foundation for diverse and visually engaging game environments, enhancing the overall gameplay experience.

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import pygame
 import sys
 from scripts.entities import PhysicsEntity
-from scripts.utils import load_image
+from scripts.utils import load_image, load_images
+from scripts.tilemap import Tilemap
 
 class Game:
     def __init__(self):
@@ -16,14 +17,22 @@ class Game:
         self.movement = [False, False]
 
         self.assets = {
+            'decor': load_images('tiles/decor'),
+            'grass': load_images('tiles/grass'),
+            'large_decor': load_images('tiles/large_decor'),
+            'stone': load_images('tiles/stone'),
             'player': load_image('entities/player.png')
         }
 
         self.player = PhysicsEntity(self, 'player', (50, 50), (8,15))
 
+        self.tilemap = Tilemap(self, tile_size=16)
+
     def run(self):
         while True:
             self.display.fill((14, 219, 248))
+
+            self.tilemap.render(self.display)
 
             self.player.update((self.movement[1] - self.movement[0], 0))
             self.player.render(self.display)
