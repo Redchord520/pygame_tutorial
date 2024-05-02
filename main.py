a86d636d3023fc06c3370d995ef73ed03aef4126
Matt Johanson <matt.johanson6@gmail.com>
2024-05-02 15:18:52 -0500

    Feature: Implement Camera Follow Functionality and Parallax Background Effect
    This commit introduces a camera system that dynamically follows the player by utilizing the scroll and offset variables. As the player moves right, the background moves left, creating a smooth follow effect. The offset variable is added to all render functions across scripts to adjust the rendering position relative to the camera's movement.
    
    To address camera stutter caused by floating-point pixel numbers, a new function called render_scroll is introduced. This function converts the scroll x and y coordinates to integers, resolving the stutter issue.
    
    A background image is imported as a new asset called 'background' and rendered using the .blit method to add depth to the game environment.
    
    Additionally, a clouds script is created, containing the Cloud and Clouds classes. The Cloud class assigns starting speed and position, and blits the cloud image onto the display. The Clouds class creates a list to store the clouds, appending them with random position variables, image choices, speeds, and depths. The clouds are sorted using sort with a lambda function to order them by depth, achieving the desired parallax effect with both the clouds and background.
    
    This comprehensive addition enhances the visual appeal of the game environment, creating a more immersive player experience.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sys
 from scripts.entities import PhysicsEntity
 from scripts.utils import load_image, load_images
 from scripts.tilemap import Tilemap
+from scripts.clouds import Clouds
 
 class Game:
     def __init__(self):
@@ -21,21 +22,34 @@ class Game:
             'grass': load_images('tiles/grass'),
             'large_decor': load_images('tiles/large_decor'),
             'stone': load_images('tiles/stone'),
-            'player': load_image('entities/player.png')
+            'player': load_image('entities/player.png'),
+            'background': load_image('background.png'),
+            'clouds': load_images('clouds')
         }
+
+        self.clouds = Clouds(self.assets['clouds'], count=16)
 
         self.player = PhysicsEntity(self, 'player', (50, 50), (8,15))
 
         self.tilemap = Tilemap(self, tile_size=16)
 
+        self.scroll = [0,0]
+
     def run(self):
         while True:
-            self.display.fill((14, 219, 248))
+            self.display.blit(self.assets['background'], (0,0))
 
-            self.tilemap.render(self.display)
+            self.scroll[0] += (self.player.rect().centerx - self.display.get_width() / 2 - self.scroll[0]) / 30
+            self.scroll[1] += (self.player.rect().centery - self.display.get_height() / 2 - self.scroll[1]) / 30
+            render_scroll = (int(self.scroll[0]), int(self.scroll[1]))
+
+            self.clouds.update()
+            self.clouds.render(self.display, offset=render_scroll)
+
+            self.tilemap.render(self.display, offset=render_scroll)
 
             self.player.update(self.tilemap, (self.movement[1] - self.movement[0], 0))
-            self.player.render(self.display)
+            self.player.render(self.display, offset=render_scroll)
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
--- scripts/entities.py
+++ scripts/entities.py
@@ -46,5 +46,5 @@ class PhysicsEntity:
         if self.collisions['down'] or self.collisions['up']:
             self.velocity[1] = 0
 
-    def render(self, surf):
-        surf.blit(self.game.assets['player'], self.pos)
+    def render(self, surf, offset=(0,0)):
+        surf.blit(self.game.assets['player'], (self.pos[0] - offset[0], self.pos[1] - offset[1]))
