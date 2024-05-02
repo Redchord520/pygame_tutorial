974136e38f18bb88066c6c682955e4233fd7fd4f
Matt Johanson <matt.johanson6@gmail.com>
2024-05-02 16:46:23 -0500

    Feature: Introduce Animation Class, Player Actions, and Enhanced Rendering
    This commit introduces an Animation class within the utils script, facilitating the setup of loop and done booleans, along with a variable for image duration. Each animation object, including player idle, run, jump, slide, and wall_slide, is instantiated as an Animation object.
    
    During the animation loop, frames are set to ensure they do not exceed the number of images for each animation object. Additionally, new functions such as action, anim_offset, flip, and set_action are defined within the PhysicsEntity class. The set_action function links the animation object asset to the action word, while flip is used to horizontally flip the player image based on movement direction.
    
    The physics_render function is updated to accommodate flip and anim_offset to maintain image padding integrity.
    
    Furthermore, a Player class is introduced, inheriting from the PhysicsEntity class. The air_time variable is added to track the duration of jumps. Movement greater than zero sets the action to run, while no movement sets the action to idle.
    
    These enhancements significantly improve animation management, player actions, and rendering quality, enhancing the overall gameplay experience.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pygame
 import sys
-from scripts.entities import PhysicsEntity
-from scripts.utils import load_image, load_images
+from scripts.entities import PhysicsEntity, Player
+from scripts.utils import load_image, load_images, Animation
 from scripts.tilemap import Tilemap
 from scripts.clouds import Clouds
 
@@ -24,12 +24,17 @@ class Game:
             'stone': load_images('tiles/stone'),
             'player': load_image('entities/player.png'),
             'background': load_image('background.png'),
-            'clouds': load_images('clouds')
+            'clouds': load_images('clouds'),
+            'player/idle': Animation(load_images('entities/player/idle'), img_dur=6),
+            'player/run': Animation(load_images('entities/player/run'), img_dur=4),
+            'player/jump': Animation(load_images('entities/player/jump')),
+            'player/slide': Animation(load_images('entities/player/slide')),
+            'player/wall_slide': Animation(load_images('entities/player/wall_slide')),
         }
 
         self.clouds = Clouds(self.assets['clouds'], count=16)
 
-        self.player = PhysicsEntity(self, 'player', (50, 50), (8,15))
+        self.player = Player(self, (50, 50), (8,15))
 
         self.tilemap = Tilemap(self, tile_size=16)
 
