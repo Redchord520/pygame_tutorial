9a20cc4983b132731d052d71241bc3a1c9c023d0
Matt Johanson <matt.johanson6@gmail.com>
2024-05-06 16:46:03 -0500

    **Feature: Introduce Level Editor with Asset Cycling, Placement, and Camera Movement**
    This commit introduces a comprehensive level editor, enhancing game development capabilities. With the editor, users can cycle through asset categories such as 'grass', 'stone', etc. using the mouse wheel. Holding shift while scrolling allows for cycling through variants.
    
    Asset placement is facilitated by left-clicking on the grid, with the option for off-grid placement by pressing 'g'. To remove assets, right-click functionality is implemented for both on-grid and off-grid assets.
    
    Camera movement around the world is enabled using the 'awsd' keys, providing users with flexible navigation options.
    
    Additionally, in the `tilemap` module, a `save` function is created to save the tilemap generated in the editor to a JSON file. A corresponding `load` function is introduced to read the JSON file and save its contents to map data.
    
    Furthermore, an autotiling algorithm is implemented in the `tilemap` module. This algorithm checks the tile and its neighbors, logically assigning the variant that should be placed, enhancing the efficiency and accuracy of tilemap creation.
    
    With these additions, the game development workflow is significantly streamlined, offering advanced tools for level creation and refinement.

--- main.py
+++ main.py
@@ -37,6 +37,7 @@ class Game:
         self.player = Player(self, (50, 50), (8,15))
 
         self.tilemap = Tilemap(self, tile_size=16)
+        self.tilemap.load('map.json')
 
         self.scroll = [0,0]
 
--- scripts/tilemap.py
+++ scripts/tilemap.py
@@ -1,7 +1,22 @@
+import json
+
 import pygame
+
+AUTOTILE_MAP = {
+    tuple(sorted([(1, 0), (0, 1)])): 0,
+    tuple(sorted([(1, 0), (0, 1), (-1, 0)])): 1,
+    tuple(sorted([(-1, 0), (0, 1)])): 2,
+    tuple(sorted([(-1, 0), (0, -1), (0, 1)])): 3,
+    tuple(sorted([(-1, 0), (0, -1)])): 4,
+    tuple(sorted([(-1, 0), (0, -1), (1, 0)])): 5,
+    tuple(sorted([(1, 0), (0, -1)])): 6,
+    tuple(sorted([(1, 0), (0, -1), (0, 1)])): 7,
+    tuple(sorted([(1, 0), (-1, 0), (0, 1), (0, -1)])): 8,
+}
 
 NEIGHBOR_OFFSETS = [(-1, 0), (-1, -1), (0, -1), (1, -1), (1, 0), (0, 0), (-1, 1), (0, 1), (1, 1)]
 PHYSICS_TILES = {'grass', 'stone'}
+AUTOTILE_TYPES = {'grass', 'stone'}
 
 class Tilemap:
     def __init__(self, game, tile_size=16):
@@ -9,10 +24,6 @@ class Tilemap:
         self.tile_size = tile_size
         self.tilemap = {}
         self.offgrid_tiles = []
-
-        for i in range(10):
-            self.tilemap[str(3 + i) + ';10'] = {'type': 'grass', 'variant': 1, 'pos': (3 + i, 10)}
-            self.tilemap['10;' + str(5 + i)] = {'type': 'stone', 'variant': 1, 'pos': (10, 5 + i)}
 
     def tiles_around(self, pos):
         tiles = []
@@ -23,12 +34,39 @@ class Tilemap:
                 tiles.append(self.tilemap[check_loc])
         return tiles
 
+    def save(self, path):
+        f = open(path, 'w')
+        json.dump({'tilemap': self.tilemap, 'tile_size': self.tile_size, 'offgrid': self.offgrid_tiles}, f)
+        f.close()
+
+    def load(self, path):
+        f = open(path, 'r')
+        map_data = json.load(f)
+        f.close()
+
+        self.tilemap = map_data['tilemap']
+        self.tile_size = map_data['tile_size']
+        self.offgrid_tiles = map_data['offgrid']
+
     def physics_rects_around(self, pos):
         rects = []
         for tile in self.tiles_around(pos):
             if tile['type'] in PHYSICS_TILES:
                 rects.append(pygame.Rect(tile['pos'][0] * self.tile_size, tile['pos'][1] * self.tile_size, self.tile_size, self.tile_size))
         return rects
+
+    def autotile(self):
+        for loc in self.tilemap:
+            tile = self.tilemap[loc]
+            neighbors = set()
+            for shift in [(1, 0), (-1, 0), (0, -1), (0, 1)]:
+                check_loc = str(tile['pos'][0] + shift[0]) + ';' + str(tile['pos'][1] + shift[1])
+                if check_loc in self.tilemap:
+                    if self.tilemap[check_loc]['type'] == tile['type']:
+                        neighbors.add(shift)
+            neighbors = tuple(sorted(neighbors))
+            if (tile['type'] in AUTOTILE_TYPES) and (neighbors in AUTOTILE_MAP):
+                tile['variant'] = AUTOTILE_MAP[neighbors]
 
     def render(self, surf, offset=(0, 0)):
         for tile in self.offgrid_tiles:
